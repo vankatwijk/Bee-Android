f4cf5a8ca2bbbeb9ecfbe33ba506f1c663855feb
sebastian24 <sebkenny@yahoo.com>
2013-11-17 18:06:16 +0100

    Project

---
+++ beeProject/beeProject/src/com/example/beeproject/FragmentStatistics.java
@@ -0,0 +1,32 @@
+package com.example.beeproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentStatistics extends Fragment {
+
+
+	public FragmentStatistics() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View rootView = inflater.inflate(R.layout.fragmentstatistics,
+				container, false);
+
+		return rootView;
+	}
+	
+}
---
+++ beeProject/beeProject/src/com/example/beeproject/GlobalVariables.java
@@ -0,0 +1,44 @@
+package com.example.beeproject;
+
+import java.util.ArrayList;
+
+public class GlobalVariables {
+
+	private static GlobalVariables mInstance= null;
+	
+	private ArrayList<Object> YardList;
+	private ArrayList<Object> HiveList;
+	
+	public GlobalVariables(){
+		YardList = new ArrayList<Object>();
+		HiveList = new ArrayList<Object>();
+	}
+	
+	public static synchronized GlobalVariables getInstance(){
+	    	if(null == mInstance){
+	    		mInstance = new GlobalVariables();
+	    	}
+	    return mInstance;
+	}
+	
+	public ArrayList<Object> getYardList(){
+		return YardList;
+	}
+	
+	public ArrayList<Object> getHiveList(){
+		return HiveList;
+	}
+	
+	public void addYard(String yardName){
+		YardObject yard = new YardObject(yardName);
+		
+		YardList.add(yard);
+	}
+	
+	public void addHive(String hiveName){
+		HiveObject hive = new HiveObject(hiveName);
+		
+		HiveList.add(hive);
+	}
+	
+}
