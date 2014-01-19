b021b48f536402fd98296293b824448999c1f2a8
Harold Salaila <h.salaila@gmail.com>
2014-01-19 17:32:26 +0100

    Moved date picker and calendar resolver to seperate file

--- android WS/BeeHappy/src/com/example/beeproject/calendar/CalendarActivity.java
+++ android WS/BeeHappy/src/com/example/beeproject/calendar/CalendarActivity.java
@@ -5,19 +5,8 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.provider.CalendarContract.Calendars;
-import android.provider.CalendarContract.Events;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.text.format.DateFormat;
@@ -25,12 +14,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.beeproject.R;
-import com.example.beeproject.global.classes.GlobalVar;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidListener;
 
@@ -41,9 +28,9 @@ public class CalendarActivity extends FragmentActivity {
 	private int _CurrentSelectedMonth = Calendar.getInstance().get(Calendar.MONTH);
 	
 	private void setCustomResourceForDates() {
-		if(!CalendarResolver.hasCalendar(getApplicationContext())) {
+		if(!BeeHappyCalendarResolver.hasCalendar(getApplicationContext())) {
 			Log.i(TAG, "Create Calendar");
-			CalendarResolver.createCalendar(getApplicationContext());
+			BeeHappyCalendarResolver.createCalendar(getApplicationContext());
 		}
 		else {
 			Log.i(TAG, "Calendar Exist");
@@ -232,130 +219,4 @@ public class CalendarActivity extends FragmentActivity {
 	        });
 		return builder.create();
 	}
-	
-	/**
-	 * Implemented version of 
-	 * http://www.derekbekoe.co.uk/blog/item/16-using-the-android-4-0-calendar-api
-	 *
-	 */
-	public static class CalendarResolver {
-		private static final String TAG = "CalendarResolver";
-		private static final String CALENDAR_NAME = "BeeHappy";
-		
-		private static final Uri CAL_URI = CalendarContract.Calendars.CONTENT_URI;
-		private static final Uri EVENT_URI = CalendarContract.Events.CONTENT_URI;
-		
-		public static String getUserId() {
-			return Integer.toString(GlobalVar.getInstance().getUserID());
-		}
-		
-		public static boolean hasCalendar(Context ctx)	{
-			final String[] CALENDARS_PROPS_PROJECTION = { Calendars._ID, Calendars.NAME, Calendars.CALENDAR_DISPLAY_NAME, 
-					Calendars.CALENDAR_TIME_ZONE, Calendars.DELETED };
-			ContentResolver cr = ctx.getContentResolver();
-			Cursor cur = cr.query(buildCalUri(), CALENDARS_PROPS_PROJECTION, Calendars.NAME + " = ?", new String[] { CALENDAR_NAME }, null);			
-			return cur.getCount() > 0;
-		}
-		
-		/**
-		 * creates a calendar
-		 * @param ctx (activity context)
-		 * @return Calendar_ID
-		 */
-		public static long createCalendar(Context ctx) {
-			ContentResolver cr = ctx.getContentResolver();
-			final ContentValues cv = buildNewCalContentValues();
-			Uri calUri = buildCalUri();
-			//insert the calendar into the database
-			cr.insert(calUri, cv);
-			Uri newUri = cr.insert(buildCalUri(), cv);
-			return Long.parseLong(newUri.getLastPathSegment());
-		}
-		
-		/**
-		 * adds an event to a calendar
-		 * @param ctx (activity context)
-		 * @param cv, ContentValues. Must contain a Calendar_ID
-		 * @return URI of the event
-		 */
-		public static Uri addEvent(Context ctx, ContentValues cv) {
-			ContentResolver cr = ctx.getContentResolver();				
-			return cr.insert(buildEventUri(), cv);
-		}
-		
-		public static int updateEvent(Context ctx, Long eventId, ContentValues cv) {
-			ContentResolver cr = ctx.getContentResolver();
-			String selection = "("+Events._ID+" = ?)";
-			String[] selectionArgs = new String[] {String.valueOf(eventId)};
-			return cr.update(buildEventUri(), cv, selection, selectionArgs);
-		}
-		
-		public static int deleteEvent(Context ctx, Long eventId) {
-			ContentResolver cr = ctx.getContentResolver();
-			String selection = "("+Events._ID+" = ?)";
-			String[] selectionArgs = new String[] {String.valueOf(eventId)};
-			return cr.delete(EVENT_URI, selection, selectionArgs);
-		}
-		
-		public static long getEvent(Context ctx, String selection, String[] selectionArgs) {			 
-			ContentResolver cr = ctx.getContentResolver();
-			final String[] PROJECTION = new String[] { Events._ID };
-			Cursor cursor = cr.query(buildEventUri(), PROJECTION, selection, selectionArgs, null);
-			return cursor.getLong(0);
-		}
-		
-		private static ContentValues buildNewCalContentValues() {
-			final ContentValues cv = new ContentValues();
-			cv.put(Calendars.ACCOUNT_NAME, getUserId());
-			cv.put(Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
-			cv.put(Calendars.NAME, CALENDAR_NAME);
-			cv.put(Calendars.CALENDAR_DISPLAY_NAME, CALENDAR_NAME);
-			cv.put(Calendars.CALENDAR_COLOR, 0xEA8561);
-			//user can only read the calendar
-			cv.put(Calendars.CALENDAR_ACCESS_LEVEL, Calendars.CAL_ACCESS_NONE);
-			cv.put(Calendars.OWNER_ACCOUNT, getUserId());
-			cv.put(Calendars.VISIBLE, 0);
-			cv.put(Calendars.SYNC_EVENTS, 0);
-			return cv;
-		}
-		
-		private static Uri buildCalUri() {
-			return CAL_URI
-					.buildUpon()
-					.appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true")
-					.appendQueryParameter(Calendars.ACCOUNT_NAME, getUserId())
-					.appendQueryParameter(Calendars.ACCOUNT_TYPE,
-							CalendarContract.ACCOUNT_TYPE_LOCAL)
-					.build();
-		}
-		
-		private static Uri buildEventUri() {
-			return EVENT_URI
-					.buildUpon()
-					.appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true")
-					.appendQueryParameter(Calendars.ACCOUNT_NAME, getUserId())
-					.appendQueryParameter(Calendars.ACCOUNT_TYPE,
-							CalendarContract.ACCOUNT_TYPE_LOCAL)
-					.build();
-		}
-	}
-	public static class DatePickerFragment extends DialogFragment
-    	implements DatePickerDialog.OnDateSetListener {
-
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			// Use the current date as the default date in the picker
-			final Calendar c = Calendar.getInstance();
-			int year = c.get(Calendar.YEAR);
-			int month = c.get(Calendar.MONTH);
-			int day = c.get(Calendar.DAY_OF_MONTH);
-			
-			// Create a new instance of DatePickerDialog and return it
-			return new DatePickerDialog(getActivity(), this, year, month, day);
-		}
-		
-		public void onDateSet(DatePicker view, int year, int month, int day) {
-		// Do something with the date chosen by the user
-		}
-}
 }
